cbb1c02063515ad48cc0e57918fd12a382d287bf
OsetrovaD <liberty_20_07@mail.ru>
2019-01-25 16:57:09 +0300

    search by java

---
+++ src/main/java/com/osetrova/explanatorydictionary/ExplanatoryDictionaryApplication.java
@@ -0,0 +1,17 @@
+package com.osetrova.explanatorydictionary;
+
+import com.osetrova.explanatorydictionary.dao.DictionaryDao;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class ExplanatoryDictionaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExplanatoryDictionaryApplication.class, args);
+
+    }
+
+}
+
